93a3e9d36fc5b0b344d3e00dc63d013b6a780255
SooraChoi <136873613+SooraChoi@users.noreply.github.com>
2023-06-20 18:55:15 +0900

    Update 11022 A+B-8.py

--- BOJ/단계별/3단계 반복문/11022 A+B-8.py
+++ BOJ/단계별/3단계 반복문/11022 A+B-8.py
@@ -12,6 +12,15 @@ for i in range(t):
   a,b=map(int, input().split())
   print(f"Case #{i+1}: {a} + {b} = {a+b}")
 
+#3 readline
+import sys
+t=int(input())
 
-#입력을 for문 안에서 받야야지, 아까처럼 for문 밖에서 ()들어오면 변수에 그 값 입력된 상태로 for문에서 돌아서 같은 값만 t번 출력
+for i in range(t):
+  a,b = map(int, sys.stdin.readline().split())
+  print(f"Case #{i+1}: {a} + {b} = {a+b}")
+
+
+# 입력을 for문 안에서 받야야지, 아까처럼 for문 밖에서 ()들어오면 변수에 그 값 입력된 상태로 for문에서 돌아서 같은 값만 t번 출력
 # i+1 대신 t 넣지마..
+# map 할 때 split 잊지말기
