a7b75ba0480750870c4b22627dc834235b68157c
SooraChoi <136873613+SooraChoi@users.noreply.github.com>
2023-06-22 00:36:59 +0900

    Update 10807 개수 세기.py

--- BOJ/단계별/4단계 1차원 배열/10807 개수 세기.py
+++ BOJ/단계별/4단계 1차원 배열/10807 개수 세기.py
@@ -5,3 +5,11 @@ arr=list(map(int,sys.stdin.readline().split()))
 f=int(input())
 
 print(arr.count(f))
+
+
+#멘토님 2가지 솔루션 다른 하나는
+cnt = 0
+for i in range(n):
+    if arr[i] == v:
+        cnt += 1
+print(cnt)
