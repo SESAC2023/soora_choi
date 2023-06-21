fc1d9b68adc3380eff5fccbeaee203cad70aa993
SooraChoi <136873613+SooraChoi@users.noreply.github.com>
2023-06-22 00:38:01 +0900

    Update 10871 x보다 작은수.py

--- BOJ/단계별/4단계 1차원 배열/10871 x보다 작은수.py
+++ BOJ/단계별/4단계 1차원 배열/10871 x보다 작은수.py
@@ -6,3 +6,12 @@ arr=list(map(int, sys.stdin.readline().split()))
 for i in (arr):
   if i<x:
     print(i)
+
+
+#멘토님
+n, x = map(int, input().split())
+arr = list(map(int, input().split()))
+
+for i in range(n):
+    if arr[i] < x:
+        print(arr[i], end=" ")
