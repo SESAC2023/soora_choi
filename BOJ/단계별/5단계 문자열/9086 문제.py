c783f699886fc706583ae38a67b61065329a358f
SooraChoi <136873613+SooraChoi@users.noreply.github.com>
2023-06-23 02:24:59 +0900

    Create 9086 문제.py

---
+++ BOJ/단계별/5단계 문자열/9086 문제.py
@@ -0,0 +1,13 @@
+T=int(input())
+
+for _ in range(T):
+  test_case=input()
+  print(test_case[0]+test_case[-1])
+
+
+#멘토님
+test_case = int(input())
+
+for tc in range(test_case):
+    data = input()
+    print(data[0] + data[-1])
