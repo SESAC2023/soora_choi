16f1c5c96843ff5676b4ec2fb088d71f0df76ce9
SooraChoi <136873613+SooraChoi@users.noreply.github.com>
2023-07-03 10:52:21 +0900

    Update 1012 x 유기농배추.py

--- BOJ/단계별/31단계 그래프와 순회/1012 유기농배추.py
+++ BOJ/단계별/31단계 그래프와 순회/1012 유기농배추.py
@@ -1,41 +1,38 @@
 #수정
 import sys
+
 sys.setrecursionlimit(int(1e6))
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-dx = [ 1, 1, -1, -1 ]
-dy = [ 1, -1, 1, -1 ]
+dx = [1, 1, -1, -1]
+dy = [1, -1, 1, -1]
 
-def dfs(x,y):
-    visited[x][y] = True
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      if not visited[nx][ny]:
-        dfs(nx,ny)
-        
-tc=int(input())
+
+def dfs(x, y):
+  visited[x][y] = True
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if nx >= 0 and ny >= 0 and nx < m and ny < n:
+      if graph[nx][ny] == 1: #안 넣어서 전체기 트루됏
+        if not visited[nx][ny]:
+          dfs(nx, ny)
+
+
+tc = int(input())
 for i in range(tc):
-    m, n, k = map(int, input().split())
-    graph = [ [0]*n for i in range(m) ] #m,n 으로 했을 때 범위 오류 떠서 바꿈.
-    visited = [ [False]*n for i in range (m) ]
-    for i in range(k):
-      a,b= map(int, input().split())
-      graph[a][b] = 1
+  m, n, k = map(int, input().split())
+  graph = [[0] * n for i in range(m)]  #m,n 으로 했을 때 범위 오류 떠서 바꿈.
+  visited = [[False] * n for i in range(m)]
+  for i in range(k):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
 
-      visited[a][b] = True
+  ans = 0
 
-        
-    ans=0
-
-    for i in range(m): # 싹 tc안으로 넣
-      for j in range(n):
-        if graph[i][j] == 1 and visited[i][j] == False:
-            ans += 1
-            dfs(i, j)
-
-    print(graph)
-    print(visited)
-    print(ans)
-
-#첫값은 뭐로 넣지?
+  for i in range(m):  # 싹 tc안으로 넣
+    for j in range(n):
+      if graph[i][j] == 1 and visited[i][j] == False:
+        ans += 1
+        dfs(i, j)
+  print(ans)
