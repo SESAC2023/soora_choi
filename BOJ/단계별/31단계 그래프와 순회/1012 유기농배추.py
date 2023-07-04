8acbb989f64c8bc1be084fe1f72e769bddb86dae
SooraChoi <136873613+SooraChoi@users.noreply.github.com>
2023-07-04 19:50:07 +0900

    Update 1012 유기농배추.py

--- BOJ/단계별/31단계 그래프와 순회/1012 유기농배추.py
+++ BOJ/단계별/31단계 그래프와 순회/1012 유기농배추.py
@@ -1,3 +1,43 @@
+#dx dy 설정 잘못함
+#😠정답
+import sys
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+def dfs(x, y):
+  visited[x][y] = True
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if nx >= 0 and ny >= 0 and nx < m and ny < n:
+      if graph[nx][ny] == 1 and visited[nx][ny] == False:
+          dfs(nx, ny)
+
+tc = int(input())
+for q in range(tc):
+  m, n, k = map(int, input().split())
+  graph = [[0] * n for i in range(m)]  #m,n 으로 했을 때 범위 오류 떠서 바꿈.
+  #print(graph)
+  visited = [[False] * n for i in range(m)]
+  #print(visited)
+  for i in range(k):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+  #print(graph)
+  
+  ans = 0
+
+  for i in range(m):  # 싹 tc안으로 넣
+    for j in range(n):
+      if graph[i][j] == 1 and visited[i][j] == False:
+        dfs(i, j)
+        ans += 1
+  print(ans)
+  
+
 #수정
 import sys
 
