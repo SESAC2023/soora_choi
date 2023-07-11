577c0134a6ee60c058282f1ace2f61ef70cb5378
SooraChoi <136873613+SooraChoi@users.noreply.github.com>
2023-07-12 07:19:53 +0900

    Update 2562* 최댓값.py

--- BOJ/단계별/4단계 1차원 배열/2562* 최댓값.py
+++ BOJ/단계별/4단계 1차원 배열/2562* 최댓값.py
@@ -1,12 +1,13 @@
-M=0
-arr=[]
-for i in range (9):
-  a=int(input())
-  arr.append(a)
-  if M<a:
-    M=a
-    i=i+1
-print(M)
-print(arr.index(M)+1)
+#한줄씩 입력받아서 리스트에 넣고
+#max=0 랑 비교해서 더 큰 값 저장
+#마지막 max의 위치찾기
+max = 0
+list=[]
 
-#i+1로 잘못접근
+for i in range(9):
+  a=int(input())
+  list.append(a)
+  if a > max :
+    max = a
+print(max)
+print(list.index(max)+1)
