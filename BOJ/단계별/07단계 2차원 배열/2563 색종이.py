48aa50b83ac1462e9b7e25f2a2d36c128ac3da95
SooraChoi <136873613+SooraChoi@users.noreply.github.com>
2023-07-25 00:28:38 +0900

    Create 2563 색종이.py

---
+++ BOJ/단계별/07단계 2차원 배열/2563 색종이.py
@@ -0,0 +1,18 @@
+#0,1,2,...,99칸
+#100*100개의 칸에 0값 주기
+list = [[0]*100 for i in range(100)]
+
+#색종이 붙은 칸은 1값 주기
+n=int(input())
+for i in range(n):
+  a,b=map(int, input().split())
+  for i in range(b,b+10):
+    for j in range(a, a+10):
+      list[i][j]=1
+
+#2차원 행렬의 합
+sum=0
+for i in list:
+  for j in i:
+    sum+=j
+print(sum)
