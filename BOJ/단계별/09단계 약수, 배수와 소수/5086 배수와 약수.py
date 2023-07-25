66aaea84d9a95e9ab9a3b345f29572a15ccc4226
SooraChoi <136873613+SooraChoi@users.noreply.github.com>
2023-07-25 17:56:53 +0900

    Create 5086 배수와 약수.py

---
+++ BOJ/단계별/09단계 약수, 배수와 소수/5086 배수와 약수.py
@@ -0,0 +1,14 @@
+while True:
+  a, b= map(int, input().split())
+  if a==0 and b==0:
+    break
+  if a > b:
+    if a%b == 0:
+      print('multiple')
+    else:
+      print('neither')
+  else:
+    if b%a == 0:
+      print('factor')
+    else:
+      print('neither')
